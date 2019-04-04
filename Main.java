a11e0993a1a2e7a0d879e2b535c185391d4865ca
BrandonRodriguezC <410brandonrodriguez@gmail.com>
2019-04-03 21:43:28 -0500

    Small fixes

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-public static int format=30;
+public static int format=10;
 	public static void main(String[] args) throws IOException {
 //		long startTime = System.nanoTime();
 		
